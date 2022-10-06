5829fb4d10bce8a126efc11b5935fd25ad1fa02e
S. Co1 <sco1.git@gmail.com>
2022-10-05 20:48:35 -0400

    Add preliminary XBM class implementation & loading methods

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # xbmini-py
-[![pre-commit](https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white)](https://github.com/pre-commit/pre-commit)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/xbmini-py)](https://pypi.org/project/xbmini-pyxbmini-py/)
+[![PyPI](https://img.shields.io/pypi/v/xbmini-py)](https://pypi.org/project/xbmini-py/)
+[![PyPI - License](https://img.shields.io/pypi/l/xbmini-py?color=magenta)](https://github.com/sco1/xbmini-py/blob/master/LICENSE)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/sco1/xbmini-py/main.svg)](https://results.pre-commit.ci/latest/github/sco1/xbmini-py/main)
 [![Code style: black](https://img.shields.io/badge/code%20style-black-black)](https://github.com/psf/black)
 [![Open in Visual Studio Code](https://img.shields.io/badge/Open%20in-VSCode.dev-blue)](https://vscode.dev/github.com/sco1/xbmini-py)
 
-Python Toolkit for the GCDC HAM.
+Python Toolkit for the GCDC HAM
--- xbmini/log_parser.py
+++ xbmini/log_parser.py
@@ -1,15 +1,23 @@
+from __future__ import annotations
+
+import datetime as dt
 import typing as t
+from dataclasses import dataclass, field
 from pathlib import Path
 
 import pandas as pd
 
-from xbmini.heading_parser import extract_header, parse_header
+from xbmini.heading_parser import HeaderInfo, extract_header, parse_header
+
+NUMERIC_T = int | float
 
 CONVERSION_GROUPS = {
     "Accel": ("accel_x", "accel_y", "accel_z"),
     "Gyro": ("gyro_x", "gyro_y", "gyro_z"),
     "Mag": ("mag_x", "mag_y", "mag_z"),
 }
+
+PRESS_TEMP_COLS = ["pressure", "temperature"]  # Pandas needs this as a list for indexing
 
 
 class HasSensitivity(t.Protocol):  # noqa: D101
@@ -20,16 +28,13 @@ def load_log(
     log_filepath: Path,
     conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
     sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
-) -> tuple[pd.DataFrame, pd.DataFrame]:
+) -> tuple[pd.DataFrame, HeaderInfo]:
     """
-    Load the provided XBM log into two `DataFrame`s: one for MPU data and one for press/temp.
+    Load data from the provided XBM log file.
 
     To work around known issues with some firmware where the sensor headers do not provide the
-    correct counts/unit conversion factor, `sensitivity_override` may be optionally specified to
-    manually provide these factors. It is expected to be a dictionary of the form
-    `<sensor name>: <sensor_info>` where `sensor name` maps to the sensor type (e.g. `"Accel"`,
-    `"Gyro"`, or `"Mag"`) and `sensor_info` is an object with a `sensivity` attribute specifying the
-    counts/unit conversion factor.
+    correct counts/unit conversion constant, `sensitivity_override` may be optionally specified to
+    manually provide these constants.
     """
     header_info = parse_header(extract_header(log_filepath))
 
@@ -42,11 +47,6 @@ def load_log(
         comment=";",
     )
 
-    # Split off temperature & pressure since they sample at a lower rate
-    press_temp = ["pressure", "temperature"]
-    press_temp_df = full_data[press_temp].dropna(axis=0)
-    full_data = full_data.drop(press_temp, axis=1, errors="ignore")
-
     # Convert measurements from raw counts to measured values
     if sensitivity_override:
         sensor_info = sensitivity_override
@@ -57,4 +57,82 @@ def load_log(
         for column in column_group:
             full_data[column] = full_data[column] / sensor_info[sensor].sensitivity
 
-    return full_data, press_temp_df
+    return full_data, header_info
+
+
+def _split_press_temp(
+    log_df: pd.DataFrame, columns: t.List[str] = PRESS_TEMP_COLS
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split off temperature & pressure columns since they sample at a lower rate.
+
+    The columns are dropped from the incoming `DataFrame`, and the modified dataframe is returned.
+    """
+    press_temp_df = log_df[columns].dropna(axis=0)
+    log_df = log_df.drop(columns, axis=1, errors="ignore")
+
+    return press_temp_df, log_df
+
+
+@dataclass(slots=True)
+class XBMLog:  # noqa: D101
+    mpu_data: pd.DataFrame
+    press_temp_data: pd.DataFrame
+    header_info: HeaderInfo
+    _is_merged: bool = False
+    _is_trimmed: bool = False
+
+    analysis_dt: dt.datetime = field(default_factory=dt.datetime.now)
+    ground_pressure: NUMERIC_T = 101_325
+    total_rigged_weight: None | NUMERIC_T = None
+
+    @classmethod
+    def from_log_file(
+        cls,
+        log_filepath: Path,
+        conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
+        sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
+    ) -> XBMLog:
+        """
+        Build a log instance from the provided log file.
+
+        To work around known issues with some firmware where the sensor headers do not provide the
+        correct counts/unit conversion constant, `sensitivity_override` may be optionally specified
+        to manually provide these constants.
+        """
+        # Since we're cheating and using the multi-load method, we have to set the merged flag back
+        # to False before returning
+        log = cls.from_multi_log((log_filepath,), conversion_groups, sensitivity_override)
+        log._is_merged = False
+
+        return log
+
+    @classmethod
+    def from_multi_log(
+        cls,
+        log_filepaths: t.Sequence[Path],
+        conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
+        sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
+    ) -> XBMLog:
+        """
+        Build a log instance by joining the provided log files.
+
+        It is assumed that all of the provided log files are from the same logging session, so they
+        share the same header information and timeseries.
+
+        To work around known issues with some firmware where the sensor headers do not provide the
+        correct counts/unit conversion constant, `sensitivity_override` may be optionally specified
+        to manually provide these constants.
+        """
+        header_info = parse_header(extract_header(log_filepaths[0]))
+        full_data = pd.concat(
+            [
+                load_log(log_file, conversion_groups, sensitivity_override)[0]
+                for log_file in log_filepaths
+            ]
+        )
+        press_temp, mpu_data = _split_press_temp(full_data)
+
+        return cls(
+            mpu_data=mpu_data, press_temp_data=press_temp, header_info=header_info, _is_merged=True
+        )
