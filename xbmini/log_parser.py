bcf0287d08f6811ce112edeab98556c28d214d3a
S. Co1 <sco1.git@gmail.com>
2022-10-06 13:29:28 -0400

    Add total acceleration calculations

--- xbmini/log_parser.py
+++ xbmini/log_parser.py
@@ -11,14 +11,17 @@ from xbmini.heading_parser import HeaderInfo, extract_header, parse_header
 
 NUMERIC_T = int | float
 
-CONVERSION_GROUPS = {
-    "Accel": ("accel_x", "accel_y", "accel_z"),
-    "Gyro": ("gyro_x", "gyro_y", "gyro_z"),
-    "Mag": ("mag_x", "mag_y", "mag_z"),
+SENSOR_GROUPS = {
+    "Accel": ["accel_x", "accel_y", "accel_z"],
+    "Gyro": ["gyro_x", "gyro_y", "gyro_z"],
+    "Mag": ["mag_x", "mag_y", "mag_z"],
+    "Quat": ["quat_x", "quat_y", "quat_z", "quat_w"],
 }
+IMU_SENSORS = ("Accel", "Gyro", "Mag")
 
 PRESS_TEMP_COLS = ["pressure", "temperature"]  # Pandas needs as a list for indexing
-QUATERNION_COLS = ["quat_x", "quat_y", "quat_z", "quat_w"]  # Pandas needs as a list for indexing
+
+ROLLING_WINDOW_WIDTH = 200
 
 
 class HasSensitivity(t.Protocol):  # noqa: D101
@@ -27,8 +30,9 @@ class HasSensitivity(t.Protocol):  # noqa: D101
 
 def load_log(
     log_filepath: Path,
-    conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
+    sensor_groups: dict[str, list[str]] = SENSOR_GROUPS,
     sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
+    rolling_window_width: int = ROLLING_WINDOW_WIDTH,
 ) -> tuple[pd.DataFrame, HeaderInfo]:
     """
     Load data from the provided XBM log file.
@@ -54,17 +58,24 @@ def load_log(
     else:
         sensor_info = header_info.sensors
 
-    for sensor, column_group in conversion_groups.items():
-        for column in column_group:
+    for sensor in IMU_SENSORS:
+        for column in sensor_groups[sensor]:
             full_data[column] = full_data[column] / sensor_info[sensor].sensitivity
+
+    # Calculate total acceleration & a rolling average
+    full_data["total_accel"] = full_data[sensor_groups["Accel"]].pow(2).sum(axis=1).pow(1 / 2)
+    full_data["total_accel_rolling"] = (
+        full_data["total_accel"].rolling(rolling_window_width, center=True).mean()
+    )
 
     # Convert temperature from mill-degree C to C
     full_data["temperature"] = full_data["temperature"] / 1000
 
     # Convert quaternion data, incoming as 16bit values, then normalize with RMS
-    full_data[QUATERNION_COLS] = full_data[QUATERNION_COLS] / 65536
-    q_rms = full_data[QUATERNION_COLS].pow(2).sum(axis=1).pow(1 / 2)
-    for col in QUATERNION_COLS:
+    quat_cols = sensor_groups["Quat"]
+    full_data[quat_cols] = full_data[quat_cols] / 65536
+    q_rms = full_data[quat_cols].pow(2).sum(axis=1).pow(1 / 2)
+    for col in quat_cols:
         # Not sure what the magic pandas invocation is do do this without a loop
         full_data[col] = full_data[col] / q_rms
 
@@ -90,6 +101,9 @@ class XBMLog:  # noqa: D101
     mpu: pd.DataFrame
     press_temp: pd.DataFrame
     header_info: HeaderInfo
+    drop_date: dt.date | None = None
+    drop_location: str | None = None
+    drop_id: str | None = None
     _is_merged: bool = False
     _is_trimmed: bool = False
 
@@ -108,6 +122,10 @@ class XBMLog:  # noqa: D101
         self.press_temp["press_alt_ft"] = self.press_temp["press_alt_m"] * 3.2808
 
     @property
+    def logger_id(self) -> str:  # noqa: D102
+        return self.header_info.serial
+
+    @property
     def ground_pressure(self) -> NUMERIC_T:  # noqa: D102
         return self._ground_pressure
 
@@ -121,8 +139,9 @@ class XBMLog:  # noqa: D101
     def from_log_file(
         cls,
         log_filepath: Path,
-        conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
+        sensor_groups: dict[str, list[str]] = SENSOR_GROUPS,
         sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
+        rolling_window_width: int = ROLLING_WINDOW_WIDTH,
     ) -> XBMLog:
         """
         Build a log instance from the provided log file.
@@ -133,7 +152,12 @@ class XBMLog:  # noqa: D101
         """
         # Since we're cheating and using the multi-load method, we have to set the merged flag back
         # to False before returning
-        log = cls.from_multi_log((log_filepath,), conversion_groups, sensitivity_override)
+        log = cls.from_multi_log(
+            log_filepaths=(log_filepath,),
+            sensor_groups=sensor_groups,
+            sensitivity_override=sensitivity_override,
+            rolling_window_width=rolling_window_width,
+        )
         log._is_merged = False
 
         return log
@@ -142,8 +166,9 @@ class XBMLog:  # noqa: D101
     def from_multi_log(
         cls,
         log_filepaths: t.Sequence[Path],
-        conversion_groups: t.Mapping[str, tuple[str, ...]] = CONVERSION_GROUPS,
+        sensor_groups: dict[str, list[str]] = SENSOR_GROUPS,
         sensitivity_override: None | t.Mapping[str, HasSensitivity] = None,
+        rolling_window_width: int = ROLLING_WINDOW_WIDTH,
     ) -> XBMLog:
         """
         Build a log instance by joining the provided log files.
@@ -158,10 +183,15 @@ class XBMLog:  # noqa: D101
         header_info = parse_header(extract_header(log_filepaths[0]))
         full_data = pd.concat(
             [
-                load_log(log_file, conversion_groups, sensitivity_override)[0]
+                load_log(log_file, sensor_groups, sensitivity_override, rolling_window_width)[0]
                 for log_file in log_filepaths
             ]
         )
         press_temp, mpu_data = _split_press_temp(full_data)
 
-        return cls(mpu=mpu_data, press_temp=press_temp, header_info=header_info, _is_merged=True)
+        return cls(
+            mpu=mpu_data,
+            press_temp=press_temp,
+            header_info=header_info,
+            _is_merged=True,
+        )
